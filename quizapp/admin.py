17cb973658f9c565ddd1d16fe0c8aed08e44c094
leoseis <eboscrib2@yahoo.com>
2025-01-22 11:46:13 +0100

    added some data

--- quizapp/admin.py
+++ quizapp/admin.py
@@ -1,5 +1,15 @@
+
 from django.contrib import admin
 from .models import Student, Question, QuestionOption
 
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('username', 'score', 'status')
 
-admin.site.register([Student, Question, QuestionOption])
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question',)
+
+@admin.register(QuestionOption)
+class QuestionOptionAdmin(admin.ModelAdmin):
+    list_display = ('option', 'is_correct', 'question')
