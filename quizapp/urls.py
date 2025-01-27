61efd3a2ddd456ca94ac5026748c28fbe76349ae
leoseis <eboscrib2@yahoo.com>
2025-01-27 11:47:35 +0100

    has taken quiz view

--- quizapp/urls.py
+++ quizapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("questions", views.questions, name="questions")
+    path("questions", views.questions, name="questions"),
+    path("has_taken_quiz/", views.has_taken_quiz, name="has_taken_quiz"),
 ]
--- quizapp/views.py
+++ quizapp/views.py
@@ -10,3 +10,13 @@ def questions(request):
     questions = Question.objects.all()
     serializer = QuestionSerializer(questions, many=True)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def has_taken_quiz(request):
+    username = request.data.get("username")
+    username = username.upper()
+    student, created = Student.objects.get_or_create(username=username)
+    if student.status == "done":
+        return Response({"error": f'{username} has taken the quiz already'}, status=403)
+    return Response({"message": "Proceed to take your quiz"})
